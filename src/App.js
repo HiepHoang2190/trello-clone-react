1077be202a5d30e60943c625a1d1e156d002202c
hieplotus <hieph@lotusoutsourcing.com>
2022-07-20 13:50:08 +0700

    Design trello Front-end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,166 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App"> 
-        <h1>
-          Hello World
-        </h1>
+    <div className="trello-trungquandev-master"> 
+       <nav className="navbar app">App Bar</nav>
+       <nav className="navbar board">Board Bar</nav>
+       <nav className="board-columns">
+         <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i.picsum.photos/id/965/200/200.jpg?hmac=OalaXaaPbpRZbMwF3uJMUdZIpUmyqse0anUnQcbStrM" alt="title-image"/>
+              Title: LOTUSDEV
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+         </div>
+         <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i.picsum.photos/id/965/200/200.jpg?hmac=OalaXaaPbpRZbMwF3uJMUdZIpUmyqse0anUnQcbStrM" alt="title-image"/>
+              Title: LOTUSDEV
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+         </div>
+         <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i.picsum.photos/id/965/200/200.jpg?hmac=OalaXaaPbpRZbMwF3uJMUdZIpUmyqse0anUnQcbStrM" alt="title-image"/>
+              Title: LOTUSDEV
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+         </div>
+         <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i.picsum.photos/id/965/200/200.jpg?hmac=OalaXaaPbpRZbMwF3uJMUdZIpUmyqse0anUnQcbStrM" alt="title-image"/>
+              Title: LOTUSDEV
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+         </div>
+         <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i.picsum.photos/id/965/200/200.jpg?hmac=OalaXaaPbpRZbMwF3uJMUdZIpUmyqse0anUnQcbStrM" alt="title-image"/>
+              Title: LOTUSDEV
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+         </div>
+         <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i.picsum.photos/id/965/200/200.jpg?hmac=OalaXaaPbpRZbMwF3uJMUdZIpUmyqse0anUnQcbStrM" alt="title-image"/>
+              Title: LOTUSDEV
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+         </div>
+         <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i.picsum.photos/id/965/200/200.jpg?hmac=OalaXaaPbpRZbMwF3uJMUdZIpUmyqse0anUnQcbStrM" alt="title-image"/>
+              Title: LOTUSDEV
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+            <li>
+              Add what you'd like to work on below
+            </li>
+          </ul>
+          <footer>Add another card</footer>
+         </div>
+       </nav>
     </div>
   );
 }
