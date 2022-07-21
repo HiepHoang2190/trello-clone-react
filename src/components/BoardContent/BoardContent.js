e27359e8d263aed9a1179df42b2421223cd66421
hieplotus <hieph@lotusoutsourcing.com>
2022-07-21 14:44:51 +0700

    Implement bootstrap, react-bootstrap,add new column

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 import { Container, Draggable } from 'react-smooth-dnd'
+import { Container as BootstrapContainer, Row, Col, Form, Button } from 'react-bootstrap'
 import { isEmpty } from 'lodash'
 
 import './BoardContent.scss'
@@ -12,7 +13,14 @@ import { initialData } from 'actions/initialData'
 export default function BoardContent() {
   const [board, setBoard] = useState({})
   const [columns, setColumns] = useState([])
+  const [openNewmColumnForm, setOpenNewColumnForm] = useState(false)
 
+  const newColumnInputRef = useRef(null)
+
+  const [newColumnTitle, setNewColumnTitle] = useState('')
+  const onNewColumnTitleChange = useCallback((e) => (
+    setNewColumnTitle(e.target.value)
+  ), [])
   useEffect(() => {
     const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
     if (boardFromDB) {
@@ -27,6 +35,14 @@ export default function BoardContent() {
       setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
     }
   }, [])
+
+  useEffect(() => {
+    if (newColumnInputRef && newColumnInputRef.current) {
+      newColumnInputRef.current.focus()
+      newColumnInputRef.current.select()
+    }
+  }, [openNewmColumnForm])
+
   if (isEmpty(board)) {
     return <div className="not-found" style={{
       'padding': '10px',
@@ -69,6 +85,37 @@ export default function BoardContent() {
     }
   }
 
+  const toggleOpenNewColumnForm = () => {
+    setOpenNewColumnForm(!openNewmColumnForm)
+  }
+
+  const addNewColumn = () => {
+    if (!newColumnTitle) {
+      newColumnInputRef.current.focus()
+      return
+    }
+    const newColumnToAdd = {
+      id: Math.random().toString(36).substr(2, 5), // 5 ranom characters, will remove when we implement code API
+      boardId: board.id,
+      title: newColumnTitle.trim(),
+      cardOrder: [],
+      cards: []
+    }
+
+    let newColumns = [...columns]
+    newColumns.push(newColumnToAdd)
+
+    let newBoard = { ...board }
+    newBoard.columnOrder = newColumns.map(c => c.id)
+    newBoard.columns = newColumns
+
+    setColumns(newColumns)
+    setBoard(newBoard)
+
+    setNewColumnTitle('')
+    toggleOpenNewColumnForm()
+  }
+
   return (
     <div className="board-content">
       <Container
@@ -91,9 +138,34 @@ export default function BoardContent() {
           )
         })}
       </Container>
-      <div className="add-new-column">
-        <i className="fa fa-plus icon"> Add another column</i>
-      </div>
+      <BootstrapContainer className="dev-trello-container">
+        {!openNewmColumnForm && (
+          <Row>
+            <Col className="add-new-column" onClick={toggleOpenNewColumnForm}>
+              <i className="fa fa-plus icon"> Add another column</i>
+            </Col>
+          </Row>
+        )}
+        {openNewmColumnForm && (
+          <Row>
+            <Col className="enter-new-column">
+              <Form.Control
+                size="sm"
+                type="text"
+                placeholder="Enter column title..."
+                className="input-enter-new-column"
+                ref={newColumnInputRef}
+                value={newColumnTitle}
+                onChange={onNewColumnTitleChange}
+                onKeyDown={event => (event.key === 'Enter') && addNewColumn()}
+              />
+              <Button variant="success" size="sm" onClick={addNewColumn}>Add column</Button>{' '}
+              <span className="cancel-new-column" onClick={toggleOpenNewColumnForm}><i className="fa fa-trash icon"></i></span>
+            </Col>
+          </Row>
+        )
+        }
+      </BootstrapContainer>
     </div>
   )
 }
